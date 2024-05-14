1a593e7b03c423fe40ea57275db47659dc836ebc
이엄지 <thumb@webzen.com>
2024-05-14 14:46:14 +0900

    feat: env, readme 설정

--- README.md
+++ README.md
@@ -1,75 +1,18 @@
-# Nuxt 3 Minimal Starter
+# SinglePageTemplate
 
-Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
+## 프로젝트 소개
 
-## Setup
+Todo List 구현 프로젝트입니다.
 
-Make sure to install the dependencies:
+## 템플릿을 사용하는 법
 
-```bash
-# npm
-npm install
+개인의 로컬 환경에 소스 코드를 직접 다운받아 사용합니다.
 
-# pnpm
-pnpm install
+-   요구사항 : `Node.js` 18.16.0
 
-# yarn
-yarn install
+## 프로젝트에 기여하는 법
 
-# bun
-bun install
-```
-
-## Development Server
-
-Start the development server on `http://localhost:3000`:
-
-```bash
-# npm
-npm run dev
-
-# pnpm
-pnpm run dev
-
-# yarn
-yarn dev
-
-# bun
-bun run dev
-```
-
-## Production
-
-Build the application for production:
-
-```bash
-# npm
-npm run build
-
-# pnpm
-pnpm run build
-
-# yarn
-yarn build
-
-# bun
-bun run build
-```
-
-Locally preview production build:
-
-```bash
-# npm
-npm run preview
-
-# pnpm
-pnpm run preview
-
-# yarn
-yarn preview
-
-# bun
-bun run preview
-```
-
-Check out the [deployment documentation](https://nuxt.com/docs/getting-started/deployment) for more information.
+1. 개인의 로컬 환경에 Clone해서 작업합니다.
+2. 작업이 완료된 후 원격 저장소를 Checkout이나 Rebase 등으로 최신화하고 대상이 되는 브랜치에 Merge합니다.
+3. Merge가 완료된 브랜치를 원격 저장소의 대상 브랜치에 Merge Request합니다.
+4. 담당자 확인 후 Merge됩니다.
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,64 +5,12 @@ export default defineNuxtConfig({
     devtools: { enabled: true },
     app: {
         head: {
-            title: "이엄지 포트폴리오",
+            title: "Todo-list",
             meta: [
                 { charset: "utf-8" },
                 {
                     name: "viewport",
                     content: "width=device-width, initial-scale=1, user-scalable=yes"
-                },
-                {
-                    property: "name",
-                    content: "이엄지 포트폴리오"
-                },
-                {
-                    property: "og:title",
-                    content: "이엄지 포트폴리오"
-                },
-                {
-                    property: "og:description",
-                    content: "Front-end 개발자 이엄지의 Portfolio"
-                },
-                {
-                    property: "description",
-                    content: "Front-end 개발자 이엄지의 Portfolio"
-                },
-                {
-                    property: "og:image",
-                    content: "https://i.ibb.co/WBrDDrY/share.jpg"
-                },
-                {
-                    property: "og:image:secure_url",
-                    content: "https://i.ibb.co/WBrDDrY/share.jpg"
-                },
-                {
-                    property: "og:image:type",
-                    content: "image/png"
-                },
-                {
-                    property: "og:image:width",
-                    content: "1280"
-                },
-                {
-                    property: "og:image:height",
-                    content: "720"
-                },
-                {
-                    property: "twitter:card",
-                    content: "summary_large_image"
-                },
-                {
-                    property: "twitter:title",
-                    content: "이엄지 포트폴리오"
-                },
-                {
-                    property: "twitter:description",
-                    content: "Front-end 개발자 이엄지의 Portfolio"
-                },
-                {
-                    property: "twitter:image",
-                    content: "https://i.ibb.co/WBrDDrY/share.jpg"
                 }
             ],
             link: [{ rel: "icon", type: "image/svg", href: "/favicon.png" }]
